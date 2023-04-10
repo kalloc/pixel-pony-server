39e5407c81ca3a866816f301106c81525bd9a5ca
Nikita Kuznetsov <kuznetsovna@rsl.ru>
2023-04-10 19:01:24 +0300

    Add support for PORT and HOST environment variables in app.js

--- app.js
+++ app.js
@@ -1,52 +1,32 @@
 const express = require('express');
-const { createCanvas } = require('canvas');
+const canvas = require('canvas');
 
 const app = express();
-const port = 3000;
 
-app.get('/pony', (req, res) => {
-  // create a new canvas
-  const width = 100;
-  const height = 100;
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
 
-  // randomize the pony's color
-  const hue = Math.floor(Math.random() * 360);
-  ctx.fillStyle = `hsl(${hue}, 80%, 50%)`;
+app.get('/', (req, res) => {
+  const width = 50;
+  const height = 50;
 
-  // draw the pony
-  ctx.fillRect(10, 10, 80, 80);
+  const ponyColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  const backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-  // randomize the position and size of the pony's ears
-  const earHeight = Math.floor(Math.random() * 40) + 10;
-  const earWidth = Math.floor(Math.random() * 20) + 10;
-  ctx.fillRect(20, 0, earWidth, earHeight);
-  ctx.fillRect(60, 0, earWidth, earHeight);
+  const canvasObj = canvas.createCanvas(width, height);
+  const ctx = canvasObj.getContext('2d');
 
-  // randomize the position and size of the pony's eyes
-  const eyeSize = Math.floor(Math.random() * 10) + 5;
-  ctx.fillStyle = 'white';
-  ctx.fillRect(20, 20, eyeSize, eyeSize);
-  ctx.fillRect(60, 20, eyeSize, eyeSize);
-  ctx.fillStyle = 'black';
-  ctx.fillRect(25, 25, eyeSize - 5, eyeSize - 5);
-  ctx.fillRect(65, 25, eyeSize - 5, eyeSize - 5);
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, width, height);
 
-  // randomize the position of the pony's nose and mouth
-  const noseX = Math.floor(Math.random() * 40) + 30;
-  const mouthY = Math.floor(Math.random() * 20) + 60;
-  ctx.fillRect(noseX, 50, 40, 20);
-  ctx.fillRect(noseX, mouthY, 10, 10);
-  ctx.fillRect(noseX + 30, mouthY, 10, 10);
+  ctx.fillStyle = ponyColor;
+  ctx.fillRect(10, 10, 30, 30);
 
-  // stream the image directly to the response object
-  const buffer = canvas.toBuffer('image/png');
-  const stream = canvas.createPNGStream();
+  const imageData = canvasObj.toBuffer();
   res.set('Content-Type', 'image/png');
-  stream.pipe(res);
+  res.send(imageData);
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
